4a74c6af336d6651ebd12c91c4e1a8303233bf83
Yoda92 <andersfisker1007@outlook.com>
2020-04-16 12:47:23 +0200

    Post method to CheckIn

--- WebApplication/Controllers/RestaurantController.cs
+++ WebApplication/Controllers/RestaurantController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
 
 namespace WebApplication.Controllers
 {
     public class RestaurantController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public RestaurantController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string inputRoomNumber, string inputAdultNumber, string inputChildrenNumber)
+        {
+            return Ok();
         }
     }
 }
