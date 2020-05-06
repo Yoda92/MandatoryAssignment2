1b44b812c7150c611ea1e41862cae4759087572e
Christoffer Nørbjerg <Chrisnorbjerg@gmail.com>
2020-05-06 15:05:59 +0200

    Fixed reservation error

--- WebApplication/Controllers/ReceptionController.cs
+++ WebApplication/Controllers/ReceptionController.cs
@@ -8,70 +8,58 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication.Data;
 using WebApplication.Models;
 
-namespace WebApplication.Controllers
-{
-    [Authorize(Policy = "ReceptionistsOnly")]
-    public class ReceptionController : Controller
-    {
+namespace WebApplication.Controllers {
+    [Authorize (Policy = "ReceptionistsOnly")]
+    public class ReceptionController : Controller {
         private readonly ApplicationDbContext _dbContext;
-        public ReceptionController(ApplicationDbContext dbContext)
-        {
+        public ReceptionController (ApplicationDbContext dbContext) {
             _dbContext = dbContext;
         }
 
-
         [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            return View(await _dbContext.Reservations.ToListAsync());
+        public async Task<IActionResult> Index () {
+            return View (await _dbContext.Reservations.ToListAsync ());
         }
 
         [HttpGet]
-        public async Task<IActionResult> Today()
-        {
-            return View("Index", await _dbContext.Reservations.Where(r => r.Date.Date == DateTime.Today).ToListAsync());
+        public async Task<IActionResult> Today () {
+            return View ("Index", await _dbContext.Reservations.Where (r => r.Date.Date == DateTime.Today).ToListAsync ());
         }
 
-
         [HttpGet]
-        public async Task<IActionResult> CheckedInToday()
-        {
+        public async Task<IActionResult> CheckedInToday () {
 
-            ViewBag.listOfReservations = await _dbContext.Reservations.Where(r => r.NumberOfAdultsCheckedIn != 0 || r.NumberOfChildrenCheckedIn != 0).ToListAsync();
+            ViewBag.listOfReservations = await _dbContext.Reservations.Where (r => r.NumberOfAdultsCheckedIn != 0 || r.NumberOfChildrenCheckedIn != 0).ToListAsync ();
 
-
-
-            ViewBag.totalCount = _dbContext.Reservations.Where(r => r.Date == DateTime.Today).ToList().Aggregate(new int(),
-                (number, reservation) =>
-                {
+            ViewBag.totalCount = _dbContext.Reservations.Where (r => r.Date == DateTime.Today).ToList ().Aggregate (new int (),
+                (number, reservation) => {
                     number += reservation.NumberOfAdultsCheckedIn + reservation.NumberOfChildrenCheckedIn;
                     return number;
                 }
 
             );
 
-            return View(ViewBag);
+            return View (ViewBag);
         }
-
 
         [HttpGet]
-        public async Task<IActionResult> GetReservationsByDay(DateTime date)
-        {
-            return View(await _dbContext.Reservations.Where(r => r.Date == date).ToListAsync());
+        public async Task<IActionResult> GetReservationsByDay (DateTime date) {
+            return View (await _dbContext.Reservations.Where (r => r.Date == date).ToListAsync ());
         }
 
-
-        //[Bind("Date,RoomNumber,NumberOfAdults,NumberOfChildren)]"
         [HttpPost]
-        public async Task<IActionResult> CreateReservation(Reservation reservation)
-        {
-            if (ModelState.IsValid)
-            {
-                _dbContext.Reservations.Add(reservation);
-                await _dbContext.SaveChangesAsync();
-                return RedirectToAction(nameof(Index), "Reception");
+        public async Task<IActionResult> CreateReservation (Reservation reservation) {
+            if (ModelState.IsValid) {
+                _dbContext.Reservations.Add (reservation);
+                await _dbContext.SaveChangesAsync ();
+                return RedirectToAction (nameof (Index), "Reception");
             }
-            return View(reservation);
+            return View (reservation);
+        }
+
+        [HttpGet]
+        public IActionResult CreateReservation () {
+            return View ();
         }
     }
 }
